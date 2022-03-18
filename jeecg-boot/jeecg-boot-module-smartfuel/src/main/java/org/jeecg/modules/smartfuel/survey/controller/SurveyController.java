5ac3a9bcd229e8d05fc090f5a576adb0585ef2b9
xiongxiong67 <934633137@qq.com>
2022-03-18 15:41:42 +0800

    煤仓数据api(GetJsonResultsByTime)返回结果，按照height倒序排序

--- jeecg-boot/jeecg-boot-module-smartfuel/src/main/java/org/jeecg/modules/smartfuel/survey/controller/SurveyController.java
+++ jeecg-boot/jeecg-boot-module-smartfuel/src/main/java/org/jeecg/modules/smartfuel/survey/controller/SurveyController.java
@@ -2,6 +2,9 @@ package org.jeecg.modules.smartfuel.survey.controller;
 
 import cn.hutool.core.io.resource.ClassPathResource;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -131,7 +135,6 @@ public class SurveyController {
     StringBuilder sb = new StringBuilder();
     try {
       String encoding = "UTF-8";
-
       ClassPathResource classPathResource = new ClassPathResource("pro/2.txt");
       InputStream inputStream = classPathResource.getStream();
       InputStreamReader read = new InputStreamReader(inputStream, encoding); // 考虑到编码格式
@@ -141,7 +144,6 @@ public class SurveyController {
         sb.append(lineTxt);
       }
       read.close();
-
     } catch (Exception e) {
       e.printStackTrace();
       return Result.error("读取文件内容出错");
@@ -164,16 +166,56 @@ public class SurveyController {
       sb1.append(replaceStr2).replace(sb1.length() - 2, sb1.length() - 1, f); // 将尾部," 替换成 }
       // 将json封装成map
       Map maps = (Map) JSON.parse(sb1.toString());
-
       for (Object map : maps.entrySet()) {
         // 遍历map取出key值并格式化，因为自定义处理的时候导致key有空格
         String key = ((Map.Entry) map).getKey().toString();
         SimpleDateFormat lsdStrFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date strD = lsdStrFormat.parse(key);
         String formatDate = lsdStrFormat.format(strD);
+
         // 如果有相同时间的key，直接返回value
         if (formatDate.equals(date)) {
-          return Result.OK(((Map.Entry) map).getValue());
+          // 处理value，每个对象按height高度倒序输出
+          JSONArray jsonArray = JSONArray.parseArray(((Map.Entry) map).getValue().toString());
+          List list = JSON.parseArray(((Map.Entry) map).getValue().toString()); // 将value转为list对象
+          List list2 = new ArrayList<>(); // 存储排序之后的结果
+
+          for (int i = 0; i < list.size(); i++) { // 遍历value list对象，处理每一条结果
+            List list1 = new ArrayList<>();
+            list1 = JSON.parseArray(list.get(i).toString()); // 存储每一条结果的list
+
+            var last = list1.get(list1.size() - 1);
+            list1.remove(list1.size() - 1); // 去除每一条结果的最后一个对象数据 {"weight": 441.0,"height": "总量"}
+
+            List<JSONObject> list3 = new ArrayList<JSONObject>();
+            for (int j = 0; j < list1.size(); j++) { // 将每一条结果转化为JSONObject对象list
+              list3.add((JSONObject) list1.get(j));
+            }
+
+            // 对JSONObject对象list中，按照height排序
+            Collections.sort(
+                list3,
+                new Comparator<JSONObject>() {
+                  private static final String KEY_NAME = "height";
+
+                  @Override
+                  public int compare(JSONObject a, JSONObject b) {
+                    BigDecimal valA = null;
+                    BigDecimal valB = null;
+                    try {
+                      valA = (BigDecimal) a.get(KEY_NAME);
+                      valB = (BigDecimal) b.get(KEY_NAME);
+                    } catch (JSONException e) {
+                      log.error("排序失败");
+                    }
+                    return -valA.compareTo(valB);
+                  }
+                });
+
+            list3.add((JSONObject) last); // 加入最后一条数据 {"weight": 441.0,"height": "总量"}
+            list2.add(list3); // 将排序好的存入返回list2中
+          }
+          return Result.OK(list2);
         }
       }
       return Result.error("没有查询到结果");
